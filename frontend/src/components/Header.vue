37dd24ed152ab625bc39cd16f7b430695bf31cac
Zoey H <zoeyh@MacBook-Pro-4.local>
2025-03-27 22:03:30 -0400

    test none dynamic import

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { RouterLink } from 'vue-router';
-import logo from '@/assets/img/logo.jpg';
+// import logo from '@/assets/img/logo.jpg';
 import LogSignButton from './LogSignButton.vue';
 import Hello from './Hello.vue';
 import store from '@/store/index.ts'
@@ -13,7 +13,7 @@ const userLoggedIn = computed(() => store.getters.isUserLoggedIn);
 <template>
     <div class="flex justify-between mt-[5vh] sm:h-20 md:h-35 lg:h-50">
         <RouterLink class="flex" to="/">
-            <img class="h-[100%] w-auto aspect-[2.97/2]" :src="logo" alt="Daily Thoughts" />
+            <img class="h-[100%] w-auto aspect-[2.97/2]" src="@/assets/img/logo.jpg" alt="Daily Thoughts" />
         </RouterLink>
         <div class="mt-[6vh]">
             <!-- If user is not logged in, show log in & Sign up button -->
