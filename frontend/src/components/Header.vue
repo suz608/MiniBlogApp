30807891ccf0b60b4163bd56c428247076bef8d0
Zoey H <zoeyh@MacBook-Pro-4.local>
2025-03-27 22:20:51 -0400

    test reason for loading delay

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink } from 'vue-router';
+// import { RouterLink } from 'vue-router';
 // import logo from '@/assets/img/logo.jpg';
 import LogSignButton from './LogSignButton.vue';
 import Hello from './Hello.vue';
@@ -12,9 +12,10 @@ const userLoggedIn = computed(() => store.getters.isUserLoggedIn);
 
 <template>
     <div class="flex justify-between mt-[5vh] sm:h-20 md:h-35 lg:h-50">
-        <RouterLink class="flex" to="/">
+        <!-- <RouterLink class="flex" to="/">
             <img class="h-[100%] w-auto aspect-[2.97/2]" src="@/assets/img/logo.jpg" alt="Daily Thoughts" />
-        </RouterLink>
+        </RouterLink> -->
+        <img class="h-[100%] w-auto aspect-[2.97/2]" src="@/assets/img/logo.jpg" alt="Daily Thoughts" />
         <div class="mt-[6vh]">
             <!-- If user is not logged in, show log in & Sign up button -->
             <LogSignButton v-if="!userLoggedIn"/>
