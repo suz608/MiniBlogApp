c67790ceed40f429b6f7edaab9693629df31925c
Zoey H <zoeyh@MacBook-Pro-4.local>
2025-05-20 16:36:54 +0800

    Adjust BottomCpn size

--- frontend/src/components/BottomCpn.vue
+++ frontend/src/components/BottomCpn.vue
@@ -2,7 +2,7 @@
 </script>
 
 <template>
-    <div class="sm:text-xs md:text-sm lg:text-md flex justify-evenly text-[rgb(150,147,147)] mt-[15vh] mb-[10vh] tracking-wider">
+    <div class="text-xs md:text-sm lg:text-md flex justify-evenly text-[rgb(150,147,147)] mt-[15vh] mb-[10vh] tracking-wider">
         <a>Terms of services | Privacy statement | locations | API | more</a>
     </div>
 </template>
